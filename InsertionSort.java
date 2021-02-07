90a69ebc7f9ae71945b6fe14926bfd967c98ffb2
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2021-02-07 14:36:47 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,3 +1,18 @@
+Write a Java program to implement the Insertion sort technique using arrays
+
+Aim : To Write a Java program to implement the Insertion sort technique using arrays
+
+Description:
+
+Insertion sort iterates, consuming one input element each repetition, and growing a sorted
+output list. Each iteration, insertion sort removes one element from the input data, finds the
+location it belongs within the sorted list, and inserts it there. It repeats until no input elements
+remain.
+
+
+
+Program:
+
 class InsertionSort
 {
     public static void main(String[] args)
@@ -24,3 +39,16 @@ class InsertionSort
             System.out.println(a[i]);
     }
 }
+
+Input and Output:
+
+Enter array size
+5
+Enter the array Elements
+5 4 1 2 23
+Elements after sorting are
+1
+2
+4
+5
+23
