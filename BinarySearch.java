c6f9eb1b141f9e1f275dc81ac9a29d4d36f16e1f
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2021-02-07 11:44:18 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,13 @@
+Write a java program to implement the Binary search technique using arrays
+
+Aim:To Write a java program to implement the Binary search technique using arrays
+
+Description:Binary search looks for searching element by comparing the middle element of the Array. If a
+match occurs, then the index of item is returned. Else If the middle element is greater than the
+searching element, then the item is searched in the sub-array to the left of the middle item.
+Otherwise, the item is searched in the sub-array to the right of the middle item. This process
+continues on the sub-array as well until the size of the sub-array reduces to zero.
+
 import java.util.*;
 class BinarySearch
 {
@@ -8,7 +18,7 @@ class BinarySearch
         int n,se,flag=0;
         System.out.println("Enter the size of sorted array");
         n = s.nextInt();
-        System.out.println("Enter the elemnts of array");
+        System.out.println("Enter the elements of array");
         for(int i=0;i<n;i++)
             a[i]=s.nextInt();
         System.out.println("Enter the search element");
@@ -35,3 +45,14 @@ class BinarySearch
             System.out.println("The search element is not found");
     }
 }
+
+Input and Output:
+
+Enter the size of sorted array
+10
+Enter the elements of array
+1 2 3 4 5 6 7 8 9 10
+Enter the search element
+5
+The search element is found at 4
+
