8e6d8a563b52b9c3b047ba0ac2bfb3d6031bd95a
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2021-02-07 15:35:38 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -7,6 +7,8 @@ Description:
 Quicksort is a divide and conquer algorithm. Quicksort first divides a large array into two
 smaller sub-arrays: the low elements and the high elements. Quicksort can then recursively sort
 the sub-arrays.
+    
+Program:    
 
 class QuickSort
 {
