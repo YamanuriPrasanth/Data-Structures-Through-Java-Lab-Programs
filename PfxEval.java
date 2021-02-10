c46fad24c2032874a0d602b4a667578bee4d3cfc
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2021-02-10 20:14:48 +0530

    Update PfxEval.java

--- PfxEval.java
+++ PfxEval.java
@@ -1,3 +1,19 @@
+Write a Java program to Evaluation of postfix expression.
+
+Aim:To Write a Java program to Evaluation of postfix expression.     
+
+Description:
+In normal algebra we use the infix notation like a+b*c. The corresponding postfix notation is abc*+. The algorithm for the conversion is as follows : 
+
+–	Scan the Postfix string from left to right. 
+–	Initialise an empty stack. 
+–	If the scannned character is an operand, add it to the stack 
+–	If the scanned character is an Operator, then pop two elements from stack. Store them to x and y. Now evaluate x operator y. Let the result of this operation be retVal. Push retVal into the stack. 
+–	Repeat the step till all the characters are scanned. 
+–	After all characters are scanned, we will have only one element in the stack. Return that element. 
+
+Program:
+
 import java.util.*;
 class Stack1
 {
@@ -52,3 +68,9 @@ class PfxEval
            System.out.println(x.pop());
        }
    }
+
+Input and Output:
+
+Enter the postfix expression
+1234*+5/-
+-1
